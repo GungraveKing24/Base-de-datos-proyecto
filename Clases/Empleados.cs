aa4225d5fcda8e033ed48a60f4285d68ff22a7aa
Gungrave <gungraveheat123@gmail.com>
2023-10-19 19:15:48 -0600

    Update

--- Clases/Empleados.cs
+++ Clases/Empleados.cs
@@ -1,18 +1,19 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Base_de_datos.Clases
 {
     internal class Empleados
     {
-        conexion conetar = new conexion();
+        conexion Conectar = new conexion();
 
         public class Empleado
         {
-            public int id {get; set;}
             public string nombre { get; set;}
             public string apellido { get; set;}
             public string nombre_usuario { get; set;}
@@ -25,7 +26,36 @@ namespace Base_de_datos.Clases
 
         public void insertar(Empleado empleado)
         {
+            try
+            {
+                string connectionString = Conectar.conectar();
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO juego (juego, estado, runN, rejugando, DatosAdicionales, Calificacion, foto) " +
+                                   "VALUES (@juego, @estado, @runN, @rejugando, @datosAdicionales, @calificacion, @foto);";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@nombre", empleado.nombre);
+                    command.Parameters.AddWithValue("@apellido", empleado.apellido);
+                    command.Parameters.AddWithValue("@", empleado.nombre_usuario);
 
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Datos insertados correctamente en la base de datos.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se pudo insertar los datos en la base de datos.");
+                    }
+                }
+            catch (Exception ex)
+            {
+                throw ex;
+                    
+            }
 
             // DateTime fecha = DateTime.Now;
         }
